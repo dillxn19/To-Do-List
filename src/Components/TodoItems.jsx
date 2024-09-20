be2c03f571e496f1c82b338c620a16c3f40e45d4
= <=>
2024-09-20 01:32:17 -0500

    54.22 GreatStack TodoList, added icons and array using useEffect to maintain

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,7 +1,18 @@
 import './CSS/TodoItems.css/'
-const TodoItems = () => {
+import tick from './Assets/tick.png'
+import not_tick from './Assets/not_tick.png'
+import cross from'./Assets/cross.png'
+
+const TodoItems = ({no,display,text}) => {
   return (
-    <div>TodoItems</div>
+    <div className='todoitems'>
+        <div className='todoitems-container'>
+            <img src={not_tick} alt="" />
+            <img src={tick} alt="" />
+            <div className='todoitems-text'>{text}</div>
+        </div>
+        <img className='todoitems-cross-icon' src={cross} alt="" />
+    </div>
   )
 }
 
